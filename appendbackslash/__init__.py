68b0799ab941cf0c5d210e891704a1d178239cd1
kreusada <67752638+kreusada@users.noreply.github.com>
2021-02-12 21:54:35 +0000

    Update __init__.py

--- appendbackslash/__init__.py
+++ appendbackslash/__init__.py
@@ -1,4 +1,11 @@
-from .appendbackslash import AppendBackslash
+from redbot.core import commands
 
+class AppendBackslash(commands.Cog):
+    def __init__(self, bot): self.bot = bot
+    
+    @commands.command(name="abs")
+    async def _abs(self, ctx, text: str): 
+        await ctx.send(f'\\{text}')
+      
 def setup(bot):
-  bot.add_cog(AppendBackslash(bot))
+    bot.add_cog(AppendBackslash(bot))
