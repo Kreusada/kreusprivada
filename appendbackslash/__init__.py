eef0db126a95ef908df2efed198c8db22a05e648
Just-Jojo <62262765+Just-Jojo@users.noreply.github.com>
2021-02-18 20:30:34 -0500

    [AppendBackslash] OwO what's this?
    yeah, no need for a cog

--- appendbackslash/__init__.py
+++ appendbackslash/__init__.py
@@ -1,15 +1,12 @@
 from redbot.core import commands
 
 
-class AppendBackslash(commands.Cog):
-    def __init__(self, bot):
-        self.bot = bot
 
-    @commands.command(name="abs")
-    async def _abs(self, ctx, text: str):
-        """Add a backslash to the start of a string."""
-        await ctx.send(f"\\{text}")
+@commands.command(name="abs")
+async def append_backslash(ctx, text: str):
+    """Add a backslash to the start of a string."""
+    await ctx.send(f"\\{text}")
 
 
 def setup(bot):
-    bot.add_cog(AppendBackslash(bot))
+    bot.add_command(append_backslash)
